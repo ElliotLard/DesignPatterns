c9ec3a7bfa8b41af2b2a39709698e95e560c990e
Chris <Chris@Chris-PC>
2016-03-20 10:57:07 -0400

    Added: comments to Pistol, Scope, TestPistol, and TestScope Updated: Tests in TestPistol

--- lab4_Decorator/src/Weapon/Scope.java
+++ lab4_Decorator/src/Weapon/Scope.java
@@ -6,9 +6,14 @@ public class Scope extends Attachment{
 		super(w);
 	}
 	
+	/**
+	 * Calculates the damage done only by the scope and returns as an int.
+	 * All values typecast to double so no accidental rounding occurs.
+	 * Final result is then typecast as an int.
+	 */
 	public int calculateDamage(int distance)
 	{
-		return (int) ((double)base.calculateDamage(distance) * ((double)(1 + (double)(base.range + 10) - distance)/(double)(base.range + 10)));
+		return (int) ((double)(base.calculateDamage(distance) * ((double)(1 +(base.range + 10) - distance)/(base.range + 10))));
 	}
 
 }
--- lab4_Decorator/tests/Weapon/TestPistol.java
+++ lab4_Decorator/tests/Weapon/TestPistol.java
@@ -7,7 +7,15 @@ import Weapon.Pistol;
 import org.junit.Test;
 
 public class TestPistol {
+	
+	/**
+	 * Lab 4 tests for Pistol
+	 */
 
+	
+	/**
+	 * Tests the creation and border cases of Pistol
+	 */
 	@Test
 	public void testInit()
 	{
@@ -17,6 +25,61 @@ public class TestPistol {
 		assertEquals(12, pistol.calculateDamage(0)); // Tests at point blank
 		assertEquals(0, pistol.calculateDamage(40)); // Tests at too far
 		assertEquals(6, pistol.calculateDamage(13)); // Tests at mid range
+	}
+	
+	/**
+	 * Tests to make sure damage is correctly calculated with a Pistol and Scope
+	 */
+	@Test
+	public void testPistolAndScope()
+	{
+		GenericWeapon p = new Pistol(); // Create pistol
+		Attachment s = new Scope(p); // Create Scope
+		
+		int damage = p.calculateDamage(13) + s.calculateDamage(13); // Adds return of calculcateDamage. Stores in damage.
+		assertEquals(9, damage); // Result should be 3 + 6 = 9
+	}
+	
+	/**
+	 * Tests to make sure damage is correctly calculated with two scopes on a pistol
+	 */
+	@Test
+	public void testPistolScopeAndScope()
+	{
+		GenericWeapon p = new Pistol(); // Create pistol
+		Attachment s = new Scope(p); // Create first scope
+		Attachment s1 = new Scope(p); // Create second scope
+		
+		int damage = p.calculateDamage(13) + s.calculateDamage(13) + s1.calculateDamage(13); // Adds three variables
+		assertEquals(12, damage); // Result should be 6 + 3 + 3 = 12
+	}
+	
+	/**
+	 * Another damage test using Scope and Stabilizer
+	 */
+	@Test
+	public void testPistolScopeAndStabalizer()
+	{
+		GenericWeapon p = new Pistol(); // Create pistol
+		Attachment s = new Scope(p); // Create Scope
+		Attachment st = new Stabilizer(p); // Create Stabilizer
+		
+		int damage = p.calculateDamage(13) + s.calculateDamage(13) + st.calculateDamage(13); // Adds three variables
+		assertEquals(16, damage); // Results should be 6 + 3 + 7 = 16
+	}
+	
+	/**
+	 * Test using Scope and Power Booster with a pistol
+	 */
+	@Test
+	public void testPistolScopeAndPowerBooster()
+	{
+		GenericWeapon p = new Pistol(); // Create Pistol
+		Attachment s = new Scope(p); // Create Scope
+		Attachment pb = new PowerBooster(p); // Create Power Booster
+		
+		int damage = p.calculateDamage(13) + s.calculateDamage(13) + pb.calculateDamage(13); // Adds three variables
+		assertEquals(21, damage); // Result should be 6 + 3 + 12 = 21
 	}
 }
 
